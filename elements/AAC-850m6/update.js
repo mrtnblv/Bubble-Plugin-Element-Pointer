31e77d1788e9913ca1e827ecd9ebbf1c51c91cf2
Martin B. Levy <git@martinblevy.com>
2024-03-25 08:49:05 +0100

    Automatic commit from Bubble

--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -1,24 +1,37 @@
 function(instance, properties, context) {
+	
+	const clickableElements = document.querySelectorAll('.clickable-element');
+    clickableElements.forEach(function(element) {
+    	element.style.removeProperty('cursor');
+	});
     
-const cursor = document.querySelector('#' + properties.id);
-const hide = properties.hide_default;
-cursor.style.position = 'absolute';
-cursor.style.zIndex = '9999';
-cursor.style.pointerEvents = 'none';
-if (hide) {
-    document.body.style.cursor = 'none';
-} else {
-    document.body.style.cursor = 'auto';
-}
-    
-const positionElement = (e)=> {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;
-   
-  cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-  
-}
+    const cursor = document.querySelector('#' + properties.id);
+    const hide = properties.hide_default;
+    cursor.style.position = 'absolute';
+    cursor.style.zIndex = '9999';
+    cursor.style.pointerEvents = 'none';
+    cursor.style.top = '0';
+	cursor.style.left = '0';
+    if (hide) {
+        document.body.classList.add('disable-pointer');
+        clickableElements.forEach(function(element) {
+            element.classList.add('disable-pointer');
+        });
+    } else {
+        document.body.classList.remove('disable-pointer');
+        clickableElements.forEach(function(element) {
+            element.classList.remove('disable-pointer');
+        });
+    }
 
-window.addEventListener('mousemove', positionElement)
+    const positionElement = (e)=> {
+      const mouseY = e.clientY;
+      const mouseX = e.clientX;
+
+      cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+    }
+
+    window.addEventListener('mousemove', positionElement)
 
 }
